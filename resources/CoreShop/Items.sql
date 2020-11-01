29b4422d820eed85e719cba65cebc12c517eb8ca
Florian <flocoaraze06390@hotmail.fr>
2020-11-01 22:16:33 +0100

    Reset Repo

--- resources/CoreShop/Items.sql
+++
@@ -1,8 +0,0 @@
-INSERT INTO `items` (`name`, `label`, `weight`, `rare`, `can_remove`) VALUES 
-    ('Pain', 'bread', '1', '0', '1'),
-    ('Eau', 'water', '1', '0', '1'),
-    ('Sandwich', 'sandwich', '1', '0', '1'),
-    ('Coca', 'coca', '1', '0', '1'),
-    ('Carte sim', 'sim', '1', '0', '1'),
-    ('Téléphone', 'phone', '1', '0', '1')
-;
--- resources/CoreShop/Stuff.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE `users_accessories` (
-  `Id` int(11) NOT NULL,
-  `Identifier` varchar(255) NOT NULL,
-  `Stuff` varchar(255) NOT NULL,
-  `Label` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
-ALTER TABLE `users_accessories` CHANGE `Id` `Id` INT(11) NOT NULL AUTO_INCREMENT, add PRIMARY KEY (`Id`); 
